bd43e17dabf0be4c89fd0105f75689bf2fc7f8d6
byron.nice <byron.nice@msd.oregonk-12.net>
2019-01-17 16:50:15 -0800

    Add gyroTurn90 java class

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/SimpleAutonmous/GyroTurn90.java
@@ -0,0 +1,31 @@
+package org.firstinspires.ftc.teamcode.Autonomous.SimpleAutonmous;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+
+import org.firstinspires.ftc.teamcode.Autonomous.Modules.Gyro;
+import org.firstinspires.ftc.teamcode.Autonomous.Modules.Wait;
+import org.firstinspires.ftc.teamcode.FTC_API.Autonomous.AutonomousBase;
+import org.firstinspires.ftc.teamcode.FTC_API.Autonomous.Modules.Module;
+import org.firstinspires.ftc.teamcode.Robot.CompRobot;
+
+
+@Autonomous
+public class GyroTurn90 extends AutonomousBase {
+    private CompRobot bot = new CompRobot();
+    private final Module[][] steps = new Module[][]{
+            {new Gyro().setTurn(90).setCalibrate(true)},
+            {new Wait().setWaitTime(1000)},
+
+    };
+
+    @Override
+    public void init() {
+        init(hardwareMap,bot,steps);
+    }
+
+    @Override
+    public void tick(){
+        telemetry.addLine("Left" + bot.drive.getLeftSideMotors()[0].getCurrentPosition() +";"+bot.drive.getLeftSideMotors()[1].getCurrentPosition());
+        telemetry.addLine("Right" + bot.drive.getRightSideMotors()[0].getCurrentPosition() +";"+bot.drive.getRightSideMotors()[1].getCurrentPosition());
+    }
+}
