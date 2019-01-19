effdd7c1552347f7e746cb3fb88305a99e0114ee
byron.nice <byron.nice@msd.oregonk-12.net>
2019-01-18 16:34:46 -0800

    Created GyroAutoSilver Javaclass

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/GyroAutoSilver.java
@@ -0,0 +1,57 @@
+package org.firstinspires.ftc.teamcode.Autonomous;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+
+import org.firstinspires.ftc.teamcode.Autonomous.Modules.AllSeeingEyeTest;
+import org.firstinspires.ftc.teamcode.Autonomous.Modules.DriveTime;
+import org.firstinspires.ftc.teamcode.Autonomous.Modules.EncoderDrive;
+import org.firstinspires.ftc.teamcode.Autonomous.Modules.Lifter;
+import org.firstinspires.ftc.teamcode.Autonomous.Modules.Wait;
+import org.firstinspires.ftc.teamcode.FTC_API.Autonomous.AutonomousBase;
+import org.firstinspires.ftc.teamcode.FTC_API.Autonomous.Modules.CallFunction;
+import org.firstinspires.ftc.teamcode.FTC_API.Autonomous.Modules.Module;
+import org.firstinspires.ftc.teamcode.Robot.CompRobot;
+import org.firstinspires.ftc.teamcode.Autonomous.Modules.Gyro;
+
+import static org.firstinspires.ftc.teamcode.Utilitys.Constants.DEFAULT_SPEED;
+import static org.firstinspires.ftc.teamcode.Utilitys.Constants.turn;
+
+
+@Autonomous
+public class GyroAutoSilver extends AutonomousBase {
+    private CompRobot bot = new CompRobot();
+    private final Module[][] steps = new Module[][]{
+            {new Lifter()},
+            {new Wait().setWaitTime(50)},
+            {new EncoderDrive().setDistances(-3,-3).setSpeed(DEFAULT_SPEED)},
+            {new Wait().setWaitTime(50)},
+            {new DriveTime().setSpeeds(0,0,4).setTime(200)},
+            {new Wait().setWaitTime(50)},
+            {new EncoderDrive().setDistances(3,3).setSpeed(DEFAULT_SPEED)},
+            {new Wait().setWaitTime(50)},
+            {new Gyro().setTurn(83).setCalibrate(true)},
+            {new AllSeeingEyeTest()},
+            {
+                    new Gyro().setTurn(19).setCalibrate(false),
+                    new Gyro().setTurn(0).setCalibrate(false),
+                    new Gyro().setTurn(-19).setCalibrate(false),
+            },
+            {
+                    new EncoderDrive().setDistances(-40,-40).setSpeed(DEFAULT_SPEED),
+                    new EncoderDrive().setDistances(-37,-37).setSpeed(DEFAULT_SPEED),
+                    new EncoderDrive().setDistances(-40,-40).setSpeed(DEFAULT_SPEED),
+            },
+    };
+
+    @Override
+    public void init() {
+        init(hardwareMap,bot,steps);
+    }
+
+    @Override
+    public void tick(){
+        telemetry.addLine("Left" + bot.drive.getLeftSideMotors()[0].getCurrentPosition() +";"+bot.drive.getLeftSideMotors()[1].getCurrentPosition());
+        telemetry.addLine("Right" + bot.drive.getRightSideMotors()[0].getCurrentPosition() +";"+bot.drive.getRightSideMotors()[1].getCurrentPosition());
+    }
+}
+
