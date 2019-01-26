8f9e321c44d9eeb0f6e0b72e86fdbe9bfe500122
Byron.Nice <byron.nice@msd.oregonk-12.net>
2019-01-26 14:27:41 -0800

    Created showAuto auto

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/ShowAuto.java
@@ -0,0 +1,40 @@
+package org.firstinspires.ftc.teamcode.Autonomous.SimpleAutonmous;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+
+import org.firstinspires.ftc.teamcode.Autonomous.Modules.AllSeeingEyeTest;
+import org.firstinspires.ftc.teamcode.Autonomous.Modules.EncoderDrive;
+import org.firstinspires.ftc.teamcode.FTC_API.Autonomous.AutonomousBase;
+import org.firstinspires.ftc.teamcode.FTC_API.Autonomous.Modules.Module;
+import org.firstinspires.ftc.teamcode.Robot.CompRobot;
+
+import static org.firstinspires.ftc.teamcode.Utilitys.Constants.DEFAULT_SPEED;
+import static org.firstinspires.ftc.teamcode.Utilitys.Constants.turn;
+
+/**
+ * Created by Byron.Nice on 1/25/2019.
+ * DESCRIPTION: Robot drives forward 12 inches.
+ */
+
+@Autonomous(name = "ShowAuto")
+public class ShowAuto extends AutonomousBase {
+    private CompRobot bot = new CompRobot();
+    private final Module [][] steps = new Module[][] {
+            {new EncoderDrive().setDistances(180,180).setSpeed(DEFAULT_SPEED)},
+            {turn(-90)},
+            {new AllSeeingEyeTest()},
+    };
+
+
+
+    @Override
+    public void init() {
+        init(hardwareMap, bot, steps);
+    }
+
+    @Override
+    public void tick(){
+        telemetry.addLine("Left" + bot.drive.getLeftSideMotors()[0].getCurrentPosition() +";"+bot.drive.getLeftSideMotors()[1].getCurrentPosition());
+        telemetry.addLine("Right" + bot.drive.getRightSideMotors()[0].getCurrentPosition() +";"+bot.drive.getRightSideMotors()[1].getCurrentPosition());
+    }
+}
