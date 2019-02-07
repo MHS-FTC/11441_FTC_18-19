0733fae4c540827f9bd677f5882a4b4e762a7184
Byron.Nice <byron.nice@msd.oregonk-12.net>
2019-02-06 18:26:39 -0800

    Created test motor program for hardware tests

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/DcMotorTestNR20.java
@@ -0,0 +1,27 @@
+/*
+package org.firstinspires.ftc.teamcode.Autonomous;
+
+
+import org.firstinspires.ftc.teamcode.FTC_API.Autonomous.AutonomousBase;
+import org.firstinspires.ftc.teamcode.FTC_API.Autonomous.Modules.Module;
+import org.firstinspires.ftc.teamcode.Robot.SubSystems.HardwareTest;
+
+public class DcMotorTestNR20 extends AutonomousBase {
+    public HardwareTest hardwareTest = new HardwareTest();
+    private final Module[][] steps = new Module[][] {
+            {new motorPowerTest ()},
+            {new motorEncoderTest ()},
+
+    };
+
+
+
+
+
+    @Override
+    public void init() {
+
+    }
+}
+
+*/
