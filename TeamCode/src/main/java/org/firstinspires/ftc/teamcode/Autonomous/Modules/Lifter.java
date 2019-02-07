f88a579e15f213c563cc1e5befbb7b5ca8a08148
Byron.Nice <byron.nice@msd.oregonk-12.net>
2019-02-06 18:28:16 -0800

    Changed rotation count to fit Bilda Motor

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/Modules/Lifter.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/Modules/Lifter.java
@@ -6,6 +6,8 @@ import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import org.firstinspires.ftc.teamcode.FTC_API.Autonomous.Modules.Module;
 import org.firstinspires.ftc.teamcode.Robot.SubSystems.Arm;
 
+import static org.firstinspires.ftc.teamcode.Utilitys.Constants.BILDA_MOTOR_TICKS_PER_REV;
+
 public class Lifter extends Module {
     private Arm lift;
     private final double ROTATIONS = -17;
@@ -16,7 +18,7 @@ public class Lifter extends Module {
     public void start() {
         lift = (Arm) robot.getSubSystem("Arm");
         lift.getMotor().setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        lift.getMotor().setTargetPosition((int) (ROTATIONS * lift.getMotor().getMotorType().getTicksPerRev()));
+        lift.getMotor().setTargetPosition((int) (ROTATIONS * BILDA_MOTOR_TICKS_PER_REV));
         lift.getMotor().setMode(DcMotor.RunMode.RUN_TO_POSITION);
         //lift.getMotor().setDirection(DcMotorSimple.Direction.REVERSE);
         lift.liftUp();
